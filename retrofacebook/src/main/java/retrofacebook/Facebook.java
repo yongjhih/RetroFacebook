c043ed5399dc7c0211c61516ba3e3d6fee2fea69
Andrew Chen <yongjhih@gmail.com>
2015-06-11 03:21:01 +0800

    typo

--- retrofacebook/src/main/java/retrofacebook/Facebook.java
+++ retrofacebook/src/main/java/retrofacebook/Facebook.java
@@ -21,7 +21,7 @@ import rx.functions.*;
 @RetroFacebook
 public abstract class Facebook {
   @RetroFacebook.GET("/{postId}")
-  public abstract Observable<Post> getPost(@RetroFacebook.Path String postId, String type);
+  public abstract Observable<Post> getPost(@RetroFacebook.Path String postId);
 
   public static Facebook create() {
       return new RetroFacebook_Facebook();
