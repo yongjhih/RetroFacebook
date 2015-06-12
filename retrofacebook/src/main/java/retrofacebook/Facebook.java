a9c8526f968ef46043e3d41711173651ea1f5916
Andrew Chen <yongjhih@gmail.com>
2015-06-12 16:08:31 +0800

    Facebook: Add logIn() and onActivityResult() for session needed api

--- retrofacebook/src/main/java/retrofacebook/Facebook.java
+++ retrofacebook/src/main/java/retrofacebook/Facebook.java
@@ -16,7 +16,19 @@
 package retrofacebook;
 
 import rx.Observable;
+import rx.Subscriber;
+
 import rx.functions.*;
+
+import com.facebook.*;
+import com.facebook.login.*;
+
+import android.content.Intent;
+import android.app.Activity;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Arrays;
 
 @RetroFacebook
 public abstract class Facebook {
@@ -47,5 +59,54 @@ public abstract class Facebook {
 
   public static Facebook create() {
       return new RetroFacebook_Facebook();
+  }
+
+  CallbackManager callbackManager;
+  Activity activity;
+
+  public Facebook initialize(Activity activity) {
+      this.activity = activity;
+
+      FacebookSdk.sdkInitialize(activity.getApplicationContext());
+      callbackManager = CallbackManager.Factory.create();
+
+      return this;
+  }
+
+  public static Facebook create(Activity activity) {
+      return create().initialize(activity);
+  }
+
+  public Observable<LoginResult> logIn() {
+      return logInWithReadPermissions(Arrays.asList("public_profile", "user_friends", "user_photos", "user_posts"));
+  }
+
+  public Observable<LoginResult> logInWithReadPermissions(final Collection<String> permissions) {
+      return Observable.create(new Observable.OnSubscribe<LoginResult>() {
+          @Override public void call(final Subscriber<? super LoginResult> sub) {
+              LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+                  @Override
+                  public void onSuccess(LoginResult loginResult) {
+                      sub.onNext(loginResult);
+                      sub.onCompleted();
+                  }
+
+                  @Override
+                  public void onCancel() {
+                      sub.onCompleted();
+                  }
+
+                  @Override
+                  public void onError(FacebookException error) {
+                      sub.onError(error);
+                  }
+              });
+              LoginManager.getInstance().logInWithReadPermissions(activity, permissions);
+          }
+      });
+  }
+
+  public void onActivityResult(int requestCode, int resultCode, Intent data) {
+      callbackManager.onActivityResult(requestCode, resultCode, data);
   }
 }
